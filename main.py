4f0414c6ddf145d15baf78e5596228801fae492f
Anish Dhane <84277723+AnuDi3745@users.noreply.github.com>
2025-05-15 22:31:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ if st.button("🔍 Detect Abbreviations"):
             start_time = time.time()
             outputs = model(**inputs)
             predictions = torch.argmax(outputs.logits, dim=2)
+            st.write(model_name)
 
 
         word_ids = encoding.word_ids()
