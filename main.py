b63462baac37a32c2898c21b1e7c4f55365ddf27
Anish Dhane <84277723+AnuDi3745@users.noreply.github.com>
2025-05-15 16:54:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -95,14 +95,16 @@ if st.button("🔍 Detect Abbreviations"):
         st.markdown("### 🧾 Tagged Sentence")
         st.markdown(styled_sentence, unsafe_allow_html=True)
 
+        time_taken = time.time()-start_time
+        st.write(time_taken)   
+
+
         try:
             save_into_sheets(text_input.strip(),time_taken, results)
         except Exception as e:
             st.error("Failed to save log to Google Sheets.")
             st.exception(e) 
             
-        time_taken = time.time()-start_time
-        st.write(time_taken)   
 
 st.markdown(
     '<a href="https://docs.google.com/spreadsheets/d/1RKW3WQ9v8KthoPPuHaUTWT32M36u88g6h8_3jOLjPwo/edit?usp=sharing" target="_blank">📊 View Logs in Google Sheets</a>',
